da594a9375575512b1b4399060059225f09046f1
brian20144201@gmail.com <YOUR@EMAIL.com>
2020-02-11 12:52:41 -0800

    message

---
+++ two-fer/two_fer.py
@@ -0,0 +1,9 @@
+def two_fer(name = None):
+     #two = ", one for me."
+    # name = one + self + two
+     if name is None:
+      return "One for you, one for me."
+     else:
+       self = "One for %s, one for me." % name 
+       return self
+
